710d2650b6b1d32ca6a26e20e6b8dbdd6ea9131f
Taras Khmelovskyi <khmelovski@gmail.com>
2017-05-06 03:08:09 +0300

    update template and gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -2,91 +2,48 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'), //scss
     nunjucksRender = require('gulp-nunjucks-render'), //do template
     plumber = require('gulp-plumber'), //show error on console
-    path = require('path');
-browserSync = require('browser-sync').create(), //browserSync
+    path = require('path'),
+    browserSync = require('browser-sync').create(), //browserSync
     htmlbeautify = require('gulp-html-beautify'),
     cleanCSS = require('gulp-clean-css'),
     uglify = require('gulp-uglify'), //minify-js
     pump = require('pump'),
+    concat = require('gulp-concat'), //img
+    cache = require('gulp-cache'), //img
+    pngquant = require('imagemin-pngquant'), //img
     imagemin = require('gulp-imagemin'), //minify image
-    // jshint = require('gulp-jshint'),//js hint
     stylish = require('jshint-stylish'), //js hint
     rename = require("gulp-rename"),
     fs = require('fs'),
+    del = require('del'),
     autoprefixer = require('gulp-autoprefixer'),
     combineMq = require('gulp-combine-mq'), //merge all @media
     sourcemaps = require('gulp-sourcemaps'); // shows in the browser links to styles line in scss
 
-// js Hint
-//minify image
-//minify js
-//minify css
-//browserSync
-//sass
-//nunjucks template + htmlbeautify
-//watcher
 
-// js Hint
-// gulp.task('jshint', function(){
-//     gulp.src('dist/js/index.js')
-//     .pipe(jshint())
-//     .pipe(jshint.reporter('jshint-stylish')); // other argument 'default'
-// })
+var sources = {
+    src: "./src/",
+    dist: "./dist/"
+};
 
-// minify image
-gulp.task('minify-image', function() {
-    return gulp.src('src/lib/img/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('dist/img'));
+//function  that we add data to all page
+function loadData(data_file) {
+    var content = fs.readFileSync(data_file);
+    return JSON.parse(content)
+}
+
+// js
+gulp.task('js', function () {
+    return gulp.src(`${sources.src}js/index.js`)
+        .pipe(gulp.dest(`${sources.dist}assets/js`))
+        .pipe(browserSync.stream()); //inject js
 });
-
-
-
-// minify js
-gulp.task('minify-js', function(cb) {
-    pump([
-            gulp.src('src/lib/*.js'),
-            uglify(),
-            rename({
-                suffix: '.min'
-            }), //add .min suffix
-            gulp.dest('dist/js')
-        ],
-        cb
-    );
-});
-
-// minify css
-gulp.task('minify-css', function() {
-    return gulp.src(['src/lib/*.css', '!src/lib/*.min.css'])
-        .pipe(plumber())
-        .pipe(cleanCSS({
-            compatibility: 'ie8'
-        }))
-        .pipe(rename({
-            suffix: '.min'
-        })) //add .min suffix
-        .pipe(gulp.dest('dist/css'));
-});
-
-//browserSync
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        },
-        ui: {
-            port: 8010
-        }
-    });
-});
-
 
 //sass
-gulp.task('sass', function() {
-    return gulp.src('./src/sass/style.scss')
+gulp.task('sass', function () {
+    return gulp.src(`${sources.src}sass/style.scss`)
         .pipe(plumber())
-        .pipe(sourcemaps.init())
+        // .pipe(sourcemaps.init())
         .pipe(sass({
             outputStyle: 'expanded'
         }).on('error', sass.logError))
@@ -97,49 +54,123 @@ gulp.task('sass', function() {
         // .pipe(combineMq({
         //   beautify: true
         // }))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./src/lib/'))
-        .pipe(gulp.dest('./dist/assets/css'))
+        // .pipe(sourcemaps.write())
+        .pipe(gulp.dest(`${sources.dist}assets/css`))
         .pipe(browserSync.stream()); //inject css
 });
 
-
-//function  that we add data   on start new page 
-function loadData(data_file) {
-    var content = fs.readFileSync(data_file);
-    return JSON.parse(content)
-}
-
 //nunjucks template + htmlbeautify
-gulp.task('nunjucks', function() {
-    return gulp.src('src/pages/**/*.html')
+gulp.task('nunjucks', function () {
+    return gulp.src(`${sources.src}pages/**/*.html`)
         .pipe(plumber())
         .pipe(nunjucksRender({
-            data: loadData('src/templates/data/template_data.json'),
-            path: ['src/templates/']
+            data: loadData(`${sources.src}templates/data/template_data.json`),
+            path: [`${sources.src}templates/`]
         }))
-        .on('error', function(err) {
+        .on('error', function (err) {
             console.log(err);
         })
         .pipe(htmlbeautify({
             indentSize: 2
         })) //html beautify
-        .pipe(gulp.dest('dist'));
+        .pipe(gulp.dest(`${sources.dist}`))
+        .on('end', browserSync.reload);
 });
 
 
 //watcher
-gulp.task('watch', function() {
+gulp.task('watch', function () {
     browserSync.init({
         server: {
             baseDir: "dist"
         }
     });
-    gulp.watch('src/sass/*.scss', ['sass']);
-    gulp.watch(['src/pages/**/*.html', 'src/templates/**/*.html'], ['nunjucks']);
-    gulp.watch(['dist/assets/js/*.js']).on('change', browserSync.reload);
-    gulp.watch(['dist/*.html']).on('change', browserSync.reload);
+    gulp.watch(`${sources.src}sass/*.scss`, ['sass']);
+    gulp.watch([`${sources.src}pages/**/*.html`, `${sources.src}templates/**/*.html`], ['nunjucks']);
+    gulp.watch([`${sources.src}js/*.js`], ['js']);
 });
 
 
-gulp.task('default', ['watch']);
+///////////////////////////////////////////////////////////////////////////////////
+
+// clear didt folder
+gulp.task('clean', function () {
+    return del.sync(`${sources.dist}*`);
+});
+
+// minify js
+gulp.task('min-js', function () {
+    return gulp.src([
+            `${sources.src}js/index.js`
+        ])
+        .pipe(concat('index.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(`${sources.dist}assets/js/`));
+});
+
+//minify css
+gulp.task('min-css', function () {
+    return gulp.src([
+            `${sources.dist}assets/css/style.css`
+        ])
+        .pipe(concat('style.min.css'))
+        .pipe(cleanCSS({
+            compatibility: 'ie9'
+        }))
+        .pipe(gulp.dest(`${sources.dist}assets/css`));
+});
+
+//minify image
+gulp.task('image-min', function () {
+    return gulp.src(`${sources.src}image/**/*`)
+        .pipe(cache(imagemin({
+            interlaced: true,
+            progressive: true,
+            optimizationLevel: 2,
+            // svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        })))
+        .pipe(gulp.dest(`${sources.dist}assets/img`));
+});
+
+
+// build minify librarys file
+gulp.task('libs-js', function () {
+    return gulp.src([
+            `!${sources.dist}assets/js/index.js`,
+            `!${sources.dist}assets/js/index.min.js`,
+            `${sources.dist}assets/js/jquery.min.js`,
+            `${sources.dist}assets/js/*.js`
+        ])
+        .pipe(concat('libs.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest(`${sources.dist}assets/js`));
+});
+
+gulp.task('libs-css', function () {
+    return gulp.src([
+            `!${sources.dist}assets/css/style.css`,
+            `!${sources.dist}assets/css/style.min.css`,
+            `${sources.dist}assets/css/bootstrap.min.css`,
+            `${sources.dist}assets/css/*.css`
+        ])
+        .pipe(concat('libs.min.css'))
+        .pipe(cleanCSS({
+            compatibility: 'ie9'
+        }))
+        .pipe(gulp.dest(`${sources.dist}assets/css`));
+});
+
+// build project
+
+gulp.task('build', ['nunjucks', 'sass'], function () {
+    var buildCssLibs = gulp.src(`${sources.src}libs/**/*.css`).pipe(gulp.dest(`${sources.dist}assets/css`));
+    var buildJsLibs = gulp.src(`${sources.src}libs/**/*.js`).pipe(gulp.dest(`${sources.dist}assets/js`));
+    var buildFonts = gulp.src(`${sources.src}fonts/**/*`).pipe(gulp.dest(`${sources.dist}assets/fonts`));
+    var buildImg = gulp.src(`${sources.src}image/**/*`).pipe(gulp.dest(`${sources.dist}assets/img`));
+    var buildJs = gulp.src(`${sources.src}js/**/*.js`).pipe(gulp.dest(`${sources.dist}assets/js`));
+});
+
+///////////////////////////////////////////////////////////////////////////////////
+
+gulp.task('default', ['build', 'watch']);
